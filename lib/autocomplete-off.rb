0e01ff049094a0adebe461f1c51a1e480278f2c6
Marco Scholl <develop@marco-scholl.de>
2011-06-08 09:28:42 +0200

    use original method #3

--- lib/autocomplete-off.rb
+++ lib/autocomplete-off.rb
@@ -1,13 +1,22 @@
+require 'erb'
+require 'active_support/concern'
+require 'action_view/helpers/capture_helper'
+require 'action_view/helpers/tag_helper'
+
 module ActionView
   module Helpers
     module TagHelper
+
+      # rename original method      
+      alias_method :tag_without_autocomplete, :tag
+      
       # This re-opens the tag method which is used by every form builder helper
       # to disable browsers autocomplete by default.
       #
       # Fields can use the browsers autocomplete by explicitly setting ":autocomplete => 'on'"
       def tag(name, options = nil, open = false, escape = true)
         options['autocomplete'] ||= 'off' if name == 'input'
-        "<#{name}#{tag_options(options, escape) if options}#{open ? ">" : " />"}".html_safe
+        tag_without_autocomplete(name, options, open, escape)
       end
     end
   end
