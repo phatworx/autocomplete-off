b1ff23477879ff30f019f202d4d7a3bf271656d7
Marco Scholl <develop@marco-scholl.de>
2011-06-08 09:48:13 +0200

    fix input type checking #1 fix nil checking #2

--- lib/autocomplete-off.rb
+++ lib/autocomplete-off.rb
@@ -7,16 +7,16 @@ module ActionView
   module Helpers
     module TagHelper
 
-      # rename original method      
-      alias_method :tag_without_autocomplete, :tag
+      # rename original method for extending     
+      alias_method :tag_without_autocomplete_off, :tag
       
       # This re-opens the tag method which is used by every form builder helper
       # to disable browsers autocomplete by default.
       #
       # Fields can use the browsers autocomplete by explicitly setting ":autocomplete => 'on'"
       def tag(name, options = nil, open = false, escape = true)
-        options['autocomplete'] ||= 'off' if name == 'input'
-        tag_without_autocomplete(name, options, open, escape)
+        options['autocomplete'] ||= 'off' if options.is_a?(Hash) and name.to_s == 'input' and ['password', 'text', 'email'].include?(options['type'].to_s)
+        tag_without_autocomplete_off(name, options, open, escape)
       end
     end
   end
